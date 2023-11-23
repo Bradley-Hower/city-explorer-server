5866c674e8ca88d89b5a2641f476b286d19c0168
Bradley Hower <bradleyhower@proton.me>
2023-11-22 21:24:14 -0800

    tweaks

--- modules/weather.js
+++ modules/weather.js
@@ -3,11 +3,11 @@ const axios = require('axios');
 
 function getWeather(req, res, next){
   const city = req.query.city;
-  const lon = req.query.lon;
   const lat = req.query.lat;
+  const lon = req.query.lon;
   const url = `https://api.weatherbit.io/v2.0/forecast/daily?city=${city}&lat=${lat}&lon=${lon}&key=${process.env.WEATHER_API_KEY}`;
   axios.get(url)
-    .then(response => response.data.results.map(forecastitem => new Forecast(forecastitem)))
+    .then(response => response.data.data.map(forecastitem => new Forecast(forecastitem)))
     .then(formattedData => res.status(200).send(formattedData))
     .catch(err => next(err));
 }
